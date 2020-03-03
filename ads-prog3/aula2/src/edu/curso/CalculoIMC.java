5363e69c2859df80a187e00391925fe3ff51801a
antoniorcn <antoniorcn@hotmail.com>
2020-03-02 21:46:40 -0300

    first commit

---
+++ ads-prog3/aula2/src/edu/curso/CalculoIMC.java
@@ -0,0 +1,29 @@
+package edu.curso;
+
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+@WebServlet("/imc")
+public class CalculoIMC extends GenericServlet {
+	public void service(
+			ServletRequest req, 
+			ServletResponse res) {
+		String peso = req.getParameter("PESO");
+		String altura = req.getParameter("ALTURA");
+		int nPeso = Integer.parseInt(peso);
+		float nAltura = Integer.parseInt(altura) / 100.0f;
+		System.out.println("Calculando IMC");
+		System.out.println("Peso: " + peso);
+		System.out.println("Altura: " + altura);
+		float imc = nPeso / (nAltura * nAltura);
+		try { 
+			PrintWriter out = res.getWriter();
+			out.println("Seu IMC é : " + imc);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
